1ad2ae0da9f59557f9aa01ba5b11af51855db65c
Toni Haka-Risku <thakarisku@qvantel.com>
2017-05-07 22:36:47 +0300

    alternative approach to pipeline.py

---
+++ pipeline_alternative.py
@@ -0,0 +1,32 @@
+""" Based on the pipeline.py we can create a higher order function to create an abstraction and bring the
+data conversion to higher level. There is a lot going on with the chained functions. It's well done but not very
+readable. Here it is only matter of taste if we should try to get more close to pure functional programming or just
+stay in a more readable level by defining named functions as in pipeline.py
+"""
+
+from functools import reduce
+
+
+def change_dict_value(_d: 'dict', key: 'str', value: 'str') -> dict:
+    dict_copy = _d.copy()
+    dict_copy[key] = value
+    return dict_copy
+
+
+def pipeline(data: 'list', functions: 'list') -> map:
+    return reduce(lambda a, x: map(x, a), functions, data)
+
+
+def action(fn, key):
+    return lambda _dict: change_dict_value(_dict, key, fn(_dict.get(key)))
+
+persons = [
+    {'firstname': 'jack', 'lastname': 'sparrow', 'msisdn': '0456571234'},
+    {'firstname': 'Willy', 'lastname': 'weedock', 'msisdn': '0400123345'},
+    {'firstname': 'Janet', 'lastname': 'Bubbles', 'msisdn': '0401235678'}
+]
+result = pipeline(persons, [action(lambda x: x.replace('.', '').title(), 'firstname'),
+                            action(lambda x: x.replace('.', '').title(), 'lastname'),
+                            action(lambda x: "+358" + x[1:], 'msisdn')])
+
+print(list(result))
