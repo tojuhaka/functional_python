de570d08ff73cd93818a87508ab18b5adfe7bcc5
Toni Haka-Risku <thakarisku@qvantel.com>
2017-05-08 21:04:27 +0300

    functionality to remove entries in pipeline

--- pipeline_alternative.py
+++ pipeline_alternative.py
@@ -7,10 +7,16 @@ stay in a more readable level by defining named functions as in pipeline.py
 from functools import reduce
 
 
-def change_dict_value(_d: 'dict', key: 'str', value: 'str') -> dict:
+def change(_d: 'dict', key: 'str', value: 'str') -> dict:
     dict_copy = _d.copy()
     dict_copy[key] = value
     return dict_copy
+
+
+def without(d, key):
+    new_d = d.copy()
+    new_d.pop(key)
+    return new_d
 
 
 def pipeline(data: 'list', functions: 'list') -> map:
@@ -18,15 +24,22 @@ def pipeline(data: 'list', functions: 'list') -> map:
 
 
 def action(fn, key):
-    return lambda _dict: change_dict_value(_dict, key, fn(_dict.get(key)))
+    return lambda _dict: change(_dict, key, fn(_dict.get(key)))
+
+
+def remove(_list: 'list'):
+    return lambda _dict: reduce(without, _list, _dict)
+
 
 persons = [
     {'firstname': 'jack', 'lastname': 'sparrow', 'msisdn': '0456571234'},
     {'firstname': 'Willy', 'lastname': 'weedock', 'msisdn': '0400123345'},
     {'firstname': 'Janet', 'lastname': 'Bubbles', 'msisdn': '0401235678'}
 ]
+
 result = pipeline(persons, [action(lambda x: x.replace('.', '').title(), 'firstname'),
                             action(lambda x: x.replace('.', '').title(), 'lastname'),
-                            action(lambda x: "+358" + x[1:], 'msisdn')])
+                            action(lambda x: "+358" + x[1:], 'msisdn'),
+                            remove(['firstname'])])
 
 print(list(result))
