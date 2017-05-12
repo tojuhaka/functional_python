62852f604d628d4cc00c5164c4532812e2b07284
Toni Haka-Risku <thakarisku@qvantel.com>
2017-05-12 10:23:20 +0300

    changed comments for race.py

--- race.py
+++ race.py
@@ -1,4 +1,4 @@
-
+""" Combining imperative and functional approach """
 from random import random
 
 start_position = (1, 1, 1)
@@ -18,8 +18,6 @@ def draw(car_positions: 'tuple') -> None:
 
 
 def race(start_position: 'tuple', time: 'int') -> tuple:
-    # recursive race is also possible here but the python stack is limited
-    # to 1000. So It's better to go with generator instead'
     for i in range(0, time):
         yield start_position
         start_position = increase_positions(start_position)
